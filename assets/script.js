4cd0039854bd5b6576d4f33862050abcdb8ac24b
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-06-20 21:19:58 -0700

    added local storage to hour values

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,15 @@
-// use class for "past", "present", and "future" to apply styles
-// to the time-block divs accordingly. (you'll need to do this in your
-// JavaScript code. Don't forget to delete this comment when you are done.
-// The classes are used as examples below and should be removed from the html
-// in the finished product.)
-var userInput = [];
-
-var text = document.getElementById(".description")
-
-init();
-
-function init() {
-    // Get stored todos from localStorage
-    // Parsing the JSON string to an object
-    var storedTasks = JSON.parse(localStorage.getItem(".description"));
-};
-    moment().calendar(null, {
-        sameDay: function (now) {
-            if (this.isBefore(now)) {
-                return '[Will Happen Today]';
-            } else {
-                return '[Happened Today]';
-
-            }
-        }
+$(document).ready(function () {
+    $(".saveBtn").on("click", function (event) {
+        var value = $(this).siblings(".description9", ".description").val();
+        // console.log("value", value);
+        var time = $(this).parent().attr("#time");
+        // console.log("time: ", time)
+        localStorage.setItem(time, value);
+        moment(value).toDate();
     });
-
-
-    function storeTask() {
-        $(".saveBtn").on("click", function () {
-        localStorage.setItem(".description", JSON.stringify(userInput));
-        event.preventDefault();
-        userInput.push(".description");
-        console.log(this)
-    });
-};
-
-    $(function () {
-        $(".saveBtn").on("click", function () {
-            var value = $(this).siblings(".description").val();
-            var time = $(this).parent().attr("#time")
-            localStorage.setItem(time, value);
-            userInput.push(".description");
-            event.preventDefault();
-            alert("You saved a task!")
-        });
-    });
+    console.log("value from local storage: ", localStorage.getItem("hour-9"));
+    $(".description9").val(localStorage.getItem("hour-9"));
+    $(".description10").val(localStorage.getItem("hour-10"));
+    $(".description11").val(localStorage.getItem("hour-11"));
+    $(".description12").val(localStorage.getItem("hour-12"));
+})
